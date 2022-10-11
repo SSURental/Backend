90e780226c5d780deef7b3e054419bf3346e2b48
HaJangWon <hos05020@gmail.com>
2022-10-11 17:46:43 +0900

    first commit

---
+++ SSU_Rental/src/main/java/com/example/SSU_Rental/SsuRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSU_Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuRentalApplication.class, args);
+	}
+
+}
---
+++ SSU_Rental/src/test/java/com/example/SSU_Rental/SsuRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSU_Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
